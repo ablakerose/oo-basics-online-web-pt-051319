b0b6d9d15a831c8437e5cc57862d7403794a2b52
Blake Rose <bdecktor@gmail.com>
2019-06-03 02:26:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,8 @@
 class Shoe
 
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, 
+  attr_accessor :color, :size, :material, :condition, :cobble
+
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
@@ -12,4 +13,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  end
 end
